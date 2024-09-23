73c7a6a41adc681c506537c02284eb1bf983f3c2
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-09-23 13:36:12 +0300

    First Trial

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet > 2500){
+    return "No can do."
+  } else if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000){
+    return "That will be twenty bucks.";
+  } else return "I will gladly take your thirty bucks."
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  if (cityName === "NYC"){
+    return "Ok, sounds good."
+  } else if (cityName !== "NYC") {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let answer = "";
+  switch (tip) {
+    case "generous":
+      answer = "Thank you so much.";
+      break;
+    case "not as generous":
+      answer = "Thank you.";
+      break;
+    default:
+      answer = "Bye."
+  }
+  return answer;
 }
